9e074cc66b20572da557fab4f23b8f008a2d256f
Sri-vishnu-r1431 <78591776+Sri-vishnu-r1431@users.noreply.github.com>
2022-05-19 12:52:17 +0530

    Update FlowControl6.java

--- Flow_Control/FlowControl6.java
+++ Flow_Control/FlowControl6.java
@@ -2,6 +2,20 @@ package admin;
 
 public class FlowControl6 {
 	public static void main(String[] args) {
-		
+		if (args.length <= 1) {
+			System.out.println("Enter details");
+		} else {
+			String gender = args[0];
+			int age = Integer.parseInt(args[1]);
+			if (gender.equalsIgnoreCase("female") && age <= 58 && age >= 1) {
+				System.out.println("The percentage of interest is 8.2%");
+			} else if (gender.equalsIgnoreCase("female")) {
+				System.out.println("The percentage of interest is 9.2%");
+			} else if (gender.equalsIgnoreCase("male") && age <= 58 && age >= 1) {
+				System.out.println("The percentage of interest is 8.4%");
+			} else if (gender.equalsIgnoreCase("male")) {
+				System.out.println("The percentage of interest is 10.5%");
+			}
+		}
 	}
 }
