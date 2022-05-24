b65e3e73c4a344ce8bc4990908b20c54e1d093ec
Sri-vishnu-r1431 <78591776+Sri-vishnu-r1431@users.noreply.github.com>
2022-05-24 07:25:38 +0530

    Create ArrayList1.java

---
+++ Collections/ArrayLists/ArrayList1.java
@@ -0,0 +1,25 @@
+package admin;
+
+import java.util.ArrayList;
+
+public class ArrayList1 {
+	public static void main(String[] args) {
+		ArrayList<String> arr=new ArrayList<String>();
+		arr.add("January");
+		arr.add("February");
+		arr.add("March");
+		arr.add("April");
+		arr.add("May");
+		arr.add("June");
+		arr.add("July");
+		arr.add("August");
+		arr.add("September");
+		arr.add("October");
+		arr.add("November");
+		arr.add("December");
+	for(String i:arr ) {
+		System.out.println(i);
+	}
+		
+	}
+}
