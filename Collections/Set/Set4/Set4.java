2496018faf1f3856aa9b38d836b64392347ca007
Sri-vishnu-r1431 <78591776+Sri-vishnu-r1431@users.noreply.github.com>
2022-05-24 07:38:37 +0530

    Create Set4.java

---
+++ Collections/Set/Set4/Set4.java
@@ -0,0 +1,13 @@
+package admin;
+
+public class Set4 {
+	public static void main(String[] args) {
+		Country country=new Country();
+		country.addCountryName("INDIA");
+		country.addCountryName("USA");
+		country.addCountryName("UK");
+		country.addCountryName("RUSSIA");
+		System.out.println("INDIA: " + country.getCountry("INDIA"));
+		System.out.println("GERMANY:"+country.getCountry("GERMANY"));
+	}
+}
